58d3acb8d345598f05bb97890f6cd8af832dffa3
dannyboi1313 <danielbsoccer13@gmail.com>
2023-10-20 10:23:55 -0600

    Accept user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        System.out.println("Testing DockerFile");
+        System.out.println("welcome to the calculator");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter your command: ");
+            String input = scanner.nextLine();
+            System.out.println("You typed "+ input);
+            if(input.contains("exit")){
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
